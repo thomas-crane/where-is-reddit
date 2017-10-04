89f8067b7c2e61e2bbf5a67e972ef8094c8ded2f
thomas-crane <thomasgcrane@gmail.com>
2017-10-04 17:09:04 +1100

    Added firebase config to environment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyAaPF6THF2otCotZrq_GTBWtXMUUc76z7o',
+    authDomain: 'where-is-reddit.firebaseapp.com',
+    databaseURL: 'https://where-is-reddit.firebaseio.com',
+    projectId: 'where-is-reddit',
+    storageBucket: 'where-is-reddit.appspot.com',
+    messagingSenderId: '447853451134'
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,5 +4,13 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyAaPF6THF2otCotZrq_GTBWtXMUUc76z7o',
+    authDomain: 'where-is-reddit.firebaseapp.com',
+    databaseURL: 'https://where-is-reddit.firebaseio.com',
+    projectId: 'where-is-reddit',
+    storageBucket: 'where-is-reddit.appspot.com',
+    messagingSenderId: '447853451134'
+  }
 };
