9669568bf1bdb69e4dfde3996980598edae7ee97
thomas-crane <thomasgcrane@gmail.com>
2017-10-04 16:57:52 +1100

    Added data service to retreive historic data

---
+++ src/app/services/data.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+import { AngularFireDatabase } from 'angularfire2/database';
+import { SubredditStatSnapshot } from '../models/subreddit-stats';
+
+@Injectable()
+export class DataService {
+
+  constructor(private db: AngularFireDatabase) { }
+
+  getHistoricalData(): Promise<SubredditStatSnapshot[]> {
+    return new Promise((resolve, reject) => {
+      const sub = this.db.list('historic', ref => ref.orderByChild('timestamp').limitToLast(12)).valueChanges();
+      console.log('Subbing');
+      sub.subscribe((value) => {
+        resolve(<SubredditStatSnapshot[]>value);
+      }, (error) => {
+        reject(error);
+      });
+    });
+  }
+}
