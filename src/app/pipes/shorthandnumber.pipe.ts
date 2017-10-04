b9253b3b66d3c610e459099957f084f1c56791b8
thomas-crane <thomasgcrane@gmail.com>
2017-10-04 13:12:23 +1100

    Added short hand number pipe for numbers

---
+++ src/app/pipes/shorthandnumber.pipe.ts
@@ -0,0 +1,22 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'shorthandnumber'
+})
+export class ShorthandnumberPipe implements PipeTransform {
+
+  transform(value: number): string {
+    if (value / 1000000000.0 >= 1) {
+      console.log('B');
+      return ((value / 1000000000.0).toFixed(2) + 'B');
+    }
+    if (value / 1000000.0 >= 1) {
+      return ((value / 1000000.0).toFixed(2) + 'M');
+    }
+    if (value / 1000.0 >= 1) {
+      return ((value / 1000.0).toFixed(2) + 'K');
+    }
+    return value.toString();
+  }
+
+}
